d63b3fc3655c0de0427cd580e8a10e5725a03dac
Ssssory <itrubchaninov@list.ru>
2021-10-30 20:28:33 +0300

    add landing

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>ЖдуЕду</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/landing/assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="/landing/assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<span class="logo"><img src="/landing/images/logo.svg" alt="" /></span>
+						<h1>Жду Еду</h1>
+						<p>Современный способ уведомления гостей ресторана о готовности заказа.</p>
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a href="#intro" class="active">Цель</a></li>
+							<li><a href="#first">Преимущества</a></li>
+							<li><a href="#second">Как это работает</a></li>
+							<li><a href="#cta">С чего начать</a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<!-- Introduction -->
+							<section id="intro" class="main">
+								<div class="spotlight">
+									<div class="content">
+										<header class="major">
+											<h2>Наша цель</h2>
+										</header>
+										<p>Дать кафе и ресторанам более современный способ общения с гостями — о готовности заказа гость узнает с помощью своего
+										телефона вместо пейджера. А вы получите инструмент для аналитики своих заказов.</p>
+										<ul class="actions">
+											<!-- <li><a href="#" class="button">Learn More</a></li> -->
+										</ul>
+									</div>
+									<span class="image"><img src="/landing/images/Mockup.jpg" alt="" /></span>
+								</div>
+							</section>
+
+						<!-- First Section -->
+							<section id="first" class="main special">
+								<header class="major">
+									<h2>Наши преимущества</h2>
+								</header>
+								<ul class="features">
+									<li>
+										
+										<h3>Гостю удобно</h3>
+										<p>Не нужно беспокоиться за то, что ты слишком далеко отошел от ресторана. Не нужно думать о лишнем предмете в кармане, вся
+										информация о заказе всегда под рукой.</p>
+									</li>
+									<li>
+										
+										<h3>Больше данных о посетителях</h3>
+										<p>Ресторан сможет работать с данными о заказах в автоматическом режиме. Сложные системы не понадобятся, все можно будет
+										получить в телефоне администратора.</p>
+									</li>
+									<li>
+										
+										<h3>Увеличение среднего чека</h3>
+										<p>Дополнительный канал взаимодействия с клиентом — автоматические рассылки по нажатию одной кнопки BETA.</p>
+									</li>
+								</ul>
+								<footer class="major">
+								</footer>
+							</section>
+
+						<!-- Second Section -->
+							<section id="second" class="main special">
+								<header class="major">
+									<h2>Как это работает?</h2>
+									<!-- <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
+									posuere. Nulla massa urna, fermentum eget quam aliquet.</p> -->
+								</header>
+								<ul class="statistics">
+									<li class="style1">
+										
+										<strong>1</strong> Заказывает
+									</li>
+									<li class="style2">
+										
+										<strong>2</strong> Сканирует qr-код
+									</li>
+									<li class="style3">
+										
+										<strong>3</strong> Добавяляет бота
+									</li>
+									<li class="style4">
+										
+										<strong>4</strong> Получает уведомление
+									</li>
+									<li class="style5">
+										
+										<strong>5</strong> Забирает заказ
+									</li>
+								</ul>
+								<!-- <p class="content">Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.</p> -->
+								<footer class="major">
+								</footer>
+							</section>
+
+						<!-- Get Started -->
+							<section id="cta" class="main special">
+								<header class="major">
+									<h2>Как начать пользоваться сервисом?</h2>
+									<p>Если вы хотите дать своим гостям более современный и приятный опыт ожидания заказа — пишите нам</p>
+								</header>
+								<footer class="major">
+								</footer>
+							</section>
+
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<section>
+							<!-- <h2>Aliquam sed mauris</h2>
+							<p>Sed lorem ipsum dolor sit amet et nullam consequat feugiat consequat magna adipiscing tempus etiam dolore veroeros. eget dapibus mauris. Cras aliquet, nisl ut viverra sollicitudin, ligula erat egestas velit, vitae tincidunt odio.</p>
+							<ul class="actions">
+								<li><a href="#" class="button">Learn More</a></li>
+							</ul> -->
+						</section>
+						<section>
+							<!-- <h2>Etiam feugiat</h2>
+							<dl class="alt">
+								<dt>Address</dt>
+								<dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
+								<dt>Phone</dt>
+								<dd>(000) 000-0000 x 0000</dd>
+								<dt>Email</dt>
+								<dd><a href="#">information@untitled.tld</a></dd>
+							</dl> -->
+							<ul class="icons">
+								<!-- <li><a href="#" class="icon brands fa-twitter alt"><span class="label">Twitter</span></a></li>
+								<li><a href="#" class="icon brands fa-facebook-f alt"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon brands fa-instagram alt"><span class="label">Instagram</span></a></li>
+								<li><a href="#" class="icon brands fa-github alt"><span class="label">GitHub</span></a></li>
+								<li><a href="#" class="icon brands fa-dribbble alt"><span class="label">Dribbble</span></a></li> -->
+							</ul>
+						</section>
+						<p class="copyright">&copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="/landing/assets/js/jquery.min.js"></script>
+			<script src="/landing/assets/js/jquery.scrollex.min.js"></script>
+			<script src="/landing/assets/js/jquery.scrolly.min.js"></script>
+			<script src="/landing/assets/js/browser.min.js"></script>
+			<script src="/landing/assets/js/breakpoints.min.js"></script>
+			<script src="/landing/assets/js/util.js"></script>
+			<script src="/landing/assets/js/main.js"></script>
+
+	</body>
+</html>
