128665ab5a1ebdfe80fe53570f8b82c0eca57068
Ssssory <itrubchaninov@list.ru>
2021-12-04 13:16:57 +0300

    add logs

---
+++ app/Http/Middleware/RequestsLogger.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+
+class RequestsLogger
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        Log::info('Incoming request:');
+        Log::info($request);
+        return $next($request);
+    }
+
+    /**
+     * Handle an outgiong request.
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return void
+     */
+    public function terminate(Request $request, JsonResponse $response)
+    {
+        Log::info('Outgoing response:');
+        Log::info($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('auth', [AuthController::class, 'auth']);
-Route::get('list', [PublicApiController::class, 'list']);
-Route::get('link', [PublicApiController::class, 'link']);
-Route::post('status', [PublicApiController::class, 'status']);
+Route::middleware(['request.log'])->group(function () {
+    Route::post('auth', [AuthController::class, 'auth']);
+    Route::get('list', [PublicApiController::class, 'list']);
+    Route::get('link', [PublicApiController::class, 'link']);
+    Route::post('status', [PublicApiController::class, 'status']);
+});
+
 
 
 
